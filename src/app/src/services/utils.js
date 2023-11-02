02d5d445f2c6fc26e99fac85a239d362e6e7f59c
Math_F_94 <mathieu_fagot@hotmail.fr>
2023-11-02 22:32:43 +0100

    test cards

--- src/app/src/services/utils.js
+++ src/app/src/services/utils.js
@@ -41,7 +41,7 @@ function dialog({title, content, hasTimeOut}) {
     const dialog = document.createElement("dialog");
     dialog.open = "open";
     dialog.prepend(titleH2);
-
+ 
     if (typeof(content) === "string")  {
         const div = document.createElement("div");
         div.innerHTML = content;
